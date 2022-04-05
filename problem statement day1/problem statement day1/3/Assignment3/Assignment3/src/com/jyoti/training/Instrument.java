f7ad8696786fb3c86af33fff1cab6ecd1065c313
atharvaborale <98140041+atharvaborale@users.noreply.github.com>
2022-04-05 22:46:23 +0530

    First day

---
+++ problem statement day1/problem statement day1/3/Assignment3/Assignment3/src/com/jyoti/training/Instrument.java
@@ -0,0 +1,5 @@
+package com.jyoti.training;
+
+public abstract class Instrument {
+	public abstract void play();
+}
---
+++ problem statement day1/problem statement day1/3/medicine-class/medicine-class/src/com/jyoti/Training/medicine.java
@@ -0,0 +1,18 @@
+package com.jyoti.Training;
+
+public class medicine{public void displayLabel(){
+	System.out.println("Company : Globex Pharma");
+System.out.println("Address : Bangalore");}}
+class Tablet extends medicine{
+	 
+public void displayLabel(){
+	System.out.println("store in a cool dry place");
+	}
+}
+class Syrup extends medicine{
+	public void displayLabel(){
+		System.out.println("Consumption as directed by thephysician");
+		}}class Ointment extends medicine{public void displayLabel(){
+			System.out.println("for external use only");
+			}
+		}
